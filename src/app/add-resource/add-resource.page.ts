603099608d7e08d76babe1215a8c577d0581dc05
Navrose Johal <johnavro@sheridancollege.ca>
2025-11-25 12:55:06 -0500

    Made the styling much better and light-hearted

--- src/app/add-resource/add-resource.page.ts
+++ src/app/add-resource/add-resource.page.ts
@@ -1,14 +1,17 @@
+
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonSelect, IonSelectOption, IonTextarea, IonButton, IonList } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonTextarea, IonButton, IonList, IonIcon } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { addIcons } from 'ionicons';
+import { addCircleOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-add-resource',
   templateUrl: 'add-resource.page.html',
   styleUrls: ['add-resource.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonInput, IonSelect, IonSelectOption, IonTextarea, IonButton, IonList, CommonModule, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonInput, IonSelect, IonSelectOption, IonTextarea, IonButton, IonList, IonIcon, CommonModule, FormsModule],
 })
 export class AddResourcePage {
   resource = {
@@ -17,7 +20,9 @@ export class AddResourcePage {
     description: ''
   };
 
-  constructor() { }
+  constructor() {
+    addIcons({ addCircleOutline });
+  }
 
   saveResource() {
     console.log('Resource saved:', this.resource);
