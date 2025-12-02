e4fbca835204fc35d0bf5374cd501fa3c0199702
Sohel Shekh <hi@sohel.tech>
2025-12-02 05:17:41 -0500

    Add notification toast component and request permission on app initialization. Updated app component to include the notification toast in the HTML and implemented permission request logic in the TypeScript file.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { NotificationToastComponent } from './components/notification-toast/notification-toast.component';
+import { NotificationService } from './services/notification.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  imports: [IonApp, IonRouterOutlet, NotificationToastComponent],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(private notificationService: NotificationService) {}
+
+  async ngOnInit() {
+    // Request browser notification permission on app start
+    await this.notificationService.requestPermission();
+  }
 }
