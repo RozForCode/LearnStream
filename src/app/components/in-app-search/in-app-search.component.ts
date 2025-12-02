7a37df1f8bf13e84a422a1cc53189149384a645f
Sohel Shekh <hi@sohel.tech>
2025-12-02 02:18:05 -0500

    Refactor in-app search component and in-app browser service. Removed unused browser controls and listeners from the search component. Updated the browser service to utilize the Capacitor Browser API instead of the InAppBrowser, simplifying the code and improving error handling.

--- src/app/components/in-app-search/in-app-search.component.ts
+++ src/app/components/in-app-search/in-app-search.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonButton, IonIcon, IonInput, IonItem, IonList } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { searchOutline, closeCircleOutline } from 'ionicons/icons';
+import { searchOutline } from 'ionicons/icons';
 import { InAppBrowserService } from '../../services/in-app-browser/in-app-browser.service';
 
 @Component({
@@ -22,13 +22,6 @@ import { InAppBrowserService } from '../../services/in-app-browser/in-app-browse
           <ion-icon name="search-outline"></ion-icon>
         </ion-button>
       </ion-item>
-      
-      <div *ngIf="isBrowserOpen" class="browser-controls">
-        <ion-button expand="block" color="danger" (click)="closeBrowser()">
-          <ion-icon name="close-circle-outline" slot="start"></ion-icon>
-          Close Browser
-        </ion-button>
-      </div>
     </ion-list>
   `,
   styles: [`
@@ -41,35 +34,22 @@ import { InAppBrowserService } from '../../services/in-app-browser/in-app-browse
       border-radius: 8px;
       margin-bottom: 10px;
     }
-    .browser-controls {
-      margin-top: 10px;
-    }
   `],
   standalone: true,
   imports: [CommonModule, FormsModule, IonButton, IonIcon, IonInput, IonItem, IonList]
 })
-export class InAppSearchComponent implements OnInit, OnDestroy {
+export class InAppSearchComponent {
   @Input() initialUrl: string = '';
   searchUrl: string = '';
-  isBrowserOpen: boolean = false;
 
   constructor(private iab: InAppBrowserService) {
-    addIcons({ searchOutline, closeCircleOutline });
+    addIcons({ searchOutline });
   }
 
   ngOnInit() {
     if (this.initialUrl) {
       this.searchUrl = this.initialUrl;
     }
-
-    this.iab.addListener('browserClosed', () => {
-      console.log('Browser closed event received');
-      this.isBrowserOpen = false;
-    });
-  }
-
-  ngOnDestroy() {
-    this.iab.removeAllListeners();
   }
 
   search() {
@@ -78,13 +58,6 @@ export class InAppSearchComponent implements OnInit, OnDestroy {
     const query = encodeURIComponent(this.searchUrl);
     const url = `https://www.google.com/search?q=${query}`;
 
-    this.isBrowserOpen = true;
     this.iab.openInWebView(url);
-  }
-
-  closeBrowser() {
-    this.iab.close();
-    // Listener will update state, but we can force it here too for responsiveness
-    this.isBrowserOpen = false;
   }
 }
--- src/app/services/in-app-browser/in-app-browser.service.ts
+++ src/app/services/in-app-browser/in-app-browser.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { InAppBrowser, AndroidViewStyle, AndroidAnimation, iOSViewStyle, iOSAnimation, DismissStyle, DefaultWebViewOptions } from '@capacitor/inappbrowser';
+import { Browser } from '@capacitor/browser';
 
 @Injectable({
     providedIn: 'root'
@@ -14,32 +14,10 @@ export class InAppBrowserService {
             return;
         }
         try {
-            await InAppBrowser.openInSystemBrowser({
-                url: url,
-                options: {
-                    android: {
-                        showTitle: true,
-                        hideToolbarOnScroll: false,
-                        viewStyle: AndroidViewStyle.BOTTOM_SHEET,
-                        bottomSheetOptions: {
-                            isFixed: false,
-                            height: 500
-                        },
-                        startAnimation: AndroidAnimation.FADE_IN,
-                        exitAnimation: AndroidAnimation.FADE_OUT
-                    },
-                    iOS: {
-                        closeButtonText: DismissStyle.DONE,
-                        viewStyle: iOSViewStyle.PAGE_SHEET,
-                        animationEffect: iOSAnimation.COVER_VERTICAL,
-                        enableBarsCollapsing: false,
-                        enableReadersMode: false
-                    }
-                }
-            });
+            await Browser.open({ url });
         } catch (error) {
-            console.error('Could not open InAppBrowser:', error);
-            window.open(url, '_system');
+            console.error('Could not open browser:', error);
+            window.open(url, '_blank');
         }
     }
 
@@ -49,28 +27,18 @@ export class InAppBrowserService {
             return;
         }
         try {
-            await InAppBrowser.openInWebView({
-                url: url,
-                options: DefaultWebViewOptions
-            });
+            await Browser.open({ url });
         } catch (error) {
             console.error('Could not open WebView:', error);
+            window.open(url, '_blank');
         }
     }
 
     async close() {
         try {
-            await InAppBrowser.close();
+            await Browser.close();
         } catch (error) {
             console.error('Could not close browser:', error);
         }
-    }
-
-    async addListener(eventName: 'browserClosed' | 'browserPageLoaded' | 'browserPageNavigationCompleted', listenerFunc: (data?: any) => void) {
-        return await InAppBrowser.addListener(eventName as any, listenerFunc);
-    }
-
-    async removeAllListeners() {
-        await InAppBrowser.removeAllListeners();
     }
 }
